3782e617cc8fd2c70c25e2ae8b04a71bfca9d114
CarlHat <tianyuan@ualberta.ca>
2020-02-11 15:38:53 -0700

    Updated lecture 9 code scripts.

---
+++ Lec4/circleinfo.m
@@ -0,0 +1,36 @@
+% CIRCLEINFO calculates the circumference and area of a circle
+% and displays them.
+%
+% Author: YOUR NAME
+% ENCMP100 B3, Winter 2020.
+% Reference: Attaway, chapter 3.1 to 3.4
+
+
+% Radius of the circle
+radius = 2;
+
+<<<<<<< HEAD
+if radius <0
+    disp(circle does not exist)
+else
+    % Calculating the circumference
+    circ = 2 * pi * radius;
+    
+    % Calculating the area
+    area = pi * radius^2;
+    
+    % Display the result
+    circ
+    area
+end
+=======
+% Calculating the circumference
+circ = 2 * pi * radius;
+
+% Calculating the area
+area = pi * radius^2;
+
+% Display the result
+circ
+area
+>>>>>>> upstream/master
